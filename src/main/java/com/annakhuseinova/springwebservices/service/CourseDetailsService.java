0340884ae95a7ad75bdc117eef6afca1cff96fa2
annakhuseinova <annakhuseinova@yandex.ru>
2021-05-21 00:31:04 +0300

    More code

--- src/main/java/com/annakhuseinova/springwebservices/service/CourseDetailsService.java
+++ src/main/java/com/annakhuseinova/springwebservices/service/CourseDetailsService.java
@@ -1,9 +1,42 @@
 package com.annakhuseinova.springwebservices.service;
 
+import com.annakhuseinova.springwebservices.model.Course;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseDetailsService {
 
+    private static List<Course> courses = new ArrayList<>();
 
+    static {
+        Course springCourse = new Course(1, "Spring", "10 steps");
+        courses.add(springCourse);
+        Course springMvcCourse = new Course(2, "Spring MVC", "10 examples");
+        courses.add(springMvcCourse);
+        Course springBootCourse = new Course(3, "Spring Boot", "6K Students");
+        courses.add(springBootCourse);
+        Course mavenCourse = new Course(4, "Maven", "Most popular course on Maven");
+        courses.add(mavenCourse);
+    }
+
+    public Optional<Course> findById(int id){
+        return courses.stream().findFirst().filter(course -> course.getId() == id);
+    }
+
+    public List<Course> findAll(){
+        return courses;
+    }
+
+    public int deleteById(int id){
+        Optional<Course> courseToDelete = courses.stream().findAny().filter(course -> course.getId() == id);
+        if (courseToDelete.isPresent()){
+            courses.remove(courseToDelete.get());
+            return 1;
+        }
+        return 0;
+    }
 }
--- src/main/java/com/annakhuseinova/springwebservices/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/annakhuseinova/springwebservices/soap/CourseDetailsEndpoint.java
@@ -1,31 +1,59 @@
 package com.annakhuseinova.springwebservices.soap;
 
-import com.annakhuseinova.springwebservices.courses.CourseDetails;
-import com.annakhuseinova.springwebservices.courses.GetCourseDetailsRequest;
-import com.annakhuseinova.springwebservices.courses.GetCourseDetailsResponse;
+import com.annakhuseinova.springwebservices.courses.*;
+import com.annakhuseinova.springwebservices.model.Course;
 import com.annakhuseinova.springwebservices.service.CourseDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.List;
+import java.util.Optional;
+
 import static com.annakhuseinova.springwebservices.constants.Constants.NAMESPACE;
 
 @Endpoint
+@RequiredArgsConstructor
 public class CourseDetailsEndpoint {
 
-    private CourseDetailsService service;
-
+    private final CourseDetailsService service;
 
     @PayloadRoot(namespace = NAMESPACE, localPart = "GetCourseDetailsRequest")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request){
-        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
-        CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("That would be a wonderful course!");
-        response.setCourseDetails(courseDetails);
+        Optional<Course> course = service.findById(request.getId());
+        return mapCourseDetails(course);
+    }
+
+    @PayloadRoot(namespace = NAMESPACE, localPart = "GetAllCourseDetailsRequest")
+    @ResponsePayload
+    public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request){
+        List<Course> allCourses = service.findAll();
+        return mapAllCourseDetails(allCourses);
+    }
+
+    private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses){
+        GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+        courses.forEach(course -> {
+            CourseDetails courseDetails = mapCourseToCourseDetails(course);
+            response.getCourseDetails().add(courseDetails);
+        });
         return response;
+    }
+
+    private GetCourseDetailsResponse mapCourseDetails(Optional<Course> course){
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        response.setCourseDetails(mapCourseToCourseDetails(course.get()));
+        return response;
+    }
+
+    private CourseDetails mapCourseToCourseDetails(Course course){
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
+        return courseDetails;
     }
 }
